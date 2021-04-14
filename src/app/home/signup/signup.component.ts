98b2588360f2ad3ccd80cd3508dd17df1cd6e6da
inacio.fabricio.nunes <inacio.fabricio.nunes@gmail.com>
2021-04-14 17:57:36 -0300

    Fim do 4º Modulo do curso Angular parte 2: Autenticação, Forms e lazy loading

---
+++ src/app/home/signup/signup.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { lowerCaseValidador } from 'src/app/shared/validators/lower-case.validador';
+import { NewUser } from './new-user';
+import { SignupService } from './signup.service';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signupForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userNotTakenValidatorService : UserNotTakenValidatorService,
+    private signupService: SignupService,
+    private router: Router
+  ) { }
+
+  ngOnInit() : void{
+    this.signupForm = this.formBuilder.group({
+      email: [
+        '', 
+        [
+          Validators.required, 
+          Validators.email
+        ]
+      ],
+      fullName: [
+        '', 
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]
+      ],
+      userName: [
+        '', 
+        [
+          Validators.required,
+          lowerCaseValidador,
+          Validators.minLength(2),
+          Validators.maxLength(30)
+        ],
+        this.userNotTakenValidatorService.checkUserNameTaken()
+      ],
+      password: [
+        '', 
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14)
+        ]
+      ]
+    });
+  }
+
+  signup(){
+    const newUser = this.signupForm.getRawValue() as NewUser;
+    this.signupService
+      .signup(newUser)
+      .subscribe(() => 
+        this.router.navigate(['']),
+        err => console.log(err)
+      );
+  }
+
+}
