258255eda9b6a9caf06693a96cecd1dff11d4109
inacio.fabricio.nunes <inacio.fabricio.nunes@gmail.com>
2021-04-16 11:41:37 -0300

    Fim do 6º modulo do curso Angular parte 3: upload, build e novos componentes

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from 'src/app/core/user/user.service';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -18,7 +20,9 @@ export class PhotoDetailsComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private photoService: PhotoService,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
@@ -26,13 +30,26 @@ export class PhotoDetailsComponent implements OnInit {
       .subscribe(params => {
         this.photoId = params.photoId;
         this.photo$ = this.photoService.findById(this.photoId);
+        this.photo$.subscribe(
+          () => {}, 
+          err => {
+            this.router.navigate(['not-found']);
+          })
       });
   }
 
   remove() {
     this.photoService
         .removePhoto(this.photoId)
-        .subscribe(() => this.router.navigate(['']));
+        .subscribe(
+          () => {
+            this.alertService.success('Photo removed!', true);
+            this.router.navigate(['/user', this.userService.getUserName()]);
+          },
+          err => {
+            console.log(err);
+            this.alertService.success('Could not delete the photo!');
+          });
   }
 
 }
